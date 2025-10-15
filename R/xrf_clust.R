510b37cb713991724eed22891c4b9508eb3d7fa9
romduc74 <romain.ducruet@gmail.com>
2025-10-15 16:19:28 +0200

    Up

--- R/xrf_clust.R
+++ R/xrf_clust.R
@@ -100,7 +100,7 @@ xrf_clust <- function(data = NULL) {
         df_denoised_total <- get_cache("df_denoised_total_cache")
         df_denoised_total$Cluster <- clustering_result$cluster
         set_cache("df_denoised_total_cache", df_denoised_total)
-        assign(df_denoised_total_cache, envir = .GlobalEnv)
+        assign("Dataframe_Clustering",df_denoised_total, envir = .GlobalEnv)
         cat("\nCluster column added to cached 'df_denoised_total'.\n\n")
         break
       }
